8a5e5e2ebae8ebd1d450971aab821a0b24a704ea
merazul2755 <merazulehsan880@gmail.com>
2022-09-11 20:10:30 +0600

    add api

--- db.js
+++ db.js
@@ -2,11 +2,13 @@ const tags= require("./data/tags");
 const videos = require("./data/videos");
 const videos2 = require("./data/videos2");
 const transactions = require("./data/transactions");
+const todos = require('./data/db_todos')
 
 module.exports = {
   tags,
   videos,
   videos2,
   transactions,
+  todos,
 }
 
