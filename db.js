42961af67c1c800c110e27cf357063806dc320c9
merazul2755 <merazulehsan880@gmail.com>
2022-09-19 22:35:36 +0600

    add db database

--- db.js
+++ db.js
@@ -2,11 +2,15 @@ const tags= require("./data/tags");
 const videos = require("./data/videos");
 const videos2 = require("./data/videos2");
 const transactions = require("./data/transactions");
+const todos = require('./data/db_todos.json')
+const db = require('./data/db.json')
 
 module.exports = {
   tags,
   videos,
   videos2,
   transactions,
+  todos,
+  db,
 }
 
