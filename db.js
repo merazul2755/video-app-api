78f79bbd6175af6c6a645a71aec353f9a0efe1f6
merazul2755 <merazulehsan880@gmail.com>
2022-09-19 22:43:20 +0600

    add db database

--- db.js
+++ db.js
@@ -2,13 +2,8 @@ const tags= require("./data/tags");
 const videos = require("./data/videos");
 const videos2 = require("./data/videos2");
 const transactions = require("./data/transactions");
-<<<<<<< HEAD
 const todos = require('./data/db_todos.json')
-const db = require('./data/db.json')
-=======
-const todos = require('./data/db_todos')
-const filters = require('./data/filters')
->>>>>>> 20d68a03b88ef9d2763d732061cf8f34dfd9449f
+const dbchat = require('./data/db.json')
 
 module.exports = {
   tags,
@@ -16,10 +11,6 @@ module.exports = {
   videos2,
   transactions,
   todos,
-<<<<<<< HEAD
-  db,
-=======
-  filters
->>>>>>> 20d68a03b88ef9d2763d732061cf8f34dfd9449f
+  dbchat,
 }
 
